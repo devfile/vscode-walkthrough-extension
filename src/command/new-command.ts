73737169c2737a8adbdd8b7e931a5e1c323e73ba
Vitaliy Gulyy <vgulyy@redhat.com>
2023-02-17 13:39:30 +0200

    Indent using spaces
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- src/command/new-command.ts
+++ src/command/new-command.ts
@@ -19,185 +19,185 @@ import { countContainerComponents } from "./util";
 @injectable()
 export class NewCommandImpl implements NewCommand {
 
-	@inject(DevfileService)
+    @inject(DevfileService)
     private service: DevfileService;
 
-	@inject(NewContainer)
-	private newContainer: NewContainer;
+    @inject(NewContainer)
+    private newContainer: NewContainer;
 
-	@inject(SaveDevfile)
-	private saveDevfile: SaveDevfile;
+    @inject(SaveDevfile)
+    private saveDevfile: SaveDevfile;
 
-	private idCounter = 0;
+    private idCounter = 0;
 
     async run(): Promise<boolean> {
-		log('NewCommandImpl::run()');
+        log('NewCommandImpl::run()');
 
-		try {
-			if (!await this.newContainer.ensureAtLeastOneContainerExist()) {
-				log('NewCommandImpl >> container is not created');
-				return;
-			}
+        try {
+            if (!await this.newContainer.ensureAtLeastOneContainerExist()) {
+                log('NewCommandImpl >> container is not created');
+                return;
+            }
 
-			// const containerComponents = countContainerComponents(this.service.getDevfile());
-			// if (containerComponents === 0) {
-			// 	await vscode.window.showErrorMessage('Something went wrong!');
-			// 	return false;
-			// }
+            // const containerComponents = countContainerComponents(this.service.getDevfile());
+            // if (containerComponents === 0) {
+            // 	await vscode.window.showErrorMessage('Something went wrong!');
+            // 	return false;
+            // }
 
-			log('>> adding a command...');
-			const command = await this.defineCommand();
-			if (command) {
-				if (!this.service.getDevfile().commands) {
-					this.service.getDevfile().commands = [];
-				}
+            log('>> adding a command...');
+            const command = await this.defineCommand();
+            if (command) {
+                if (!this.service.getDevfile().commands) {
+                    this.service.getDevfile().commands = [];
+                }
 
-				this.service.getDevfile().commands.push(command);
-				
-				await this.saveDevfile.onDidDevfileUpdate();
-				vscode.window.showInformationMessage(`Command '${command.id}' has been created successfully`, 'Open Devfile');
-				return true;
-			}
+                this.service.getDevfile().commands.push(command);
 
-			log('<< canceled');
+                await this.saveDevfile.onDidDevfileUpdate();
+                vscode.window.showInformationMessage(`Command '${command.id}' has been created successfully`, 'Open Devfile');
+                return true;
+            }
+
+            log('<< canceled');
 
         } catch (err) {
             log(`ERROR occured: ${err.message}`);
         }
 
-		return false;
+        return false;
     }
 
-	private async defineCommand(): Promise<devfile.Command | undefined> {
-		log('NewCommandImpl::defineCommand()');
+    private async defineCommand(): Promise<devfile.Command | undefined> {
+        log('NewCommandImpl::defineCommand()');
 
-		const label = await this.enterLabel();
-		log(`>> label ${label}`);
-		if (!label) {
-			return undefined;
-		}
+        const label = await this.enterLabel();
+        log(`>> label ${label}`);
+        if (!label) {
+            return undefined;
+        }
 
-		const component = await this.selectComponent();
-		log(`>> component ${component}`);
-		if (!component) {
-			return undefined;
-		}
+        const component = await this.selectComponent();
+        log(`>> component ${component}`);
+        if (!component) {
+            return undefined;
+        }
 
-		const commandLine = await this.enterCommandLine();
-		log(`>> command line ${commandLine}`);
-		if (!commandLine) {
-			return undefined;
-		}
+        const commandLine = await this.enterCommandLine();
+        log(`>> command line ${commandLine}`);
+        if (!commandLine) {
+            return undefined;
+        }
 
-		// form command ID
-		let commandID;
-		do {
-			this.idCounter++;
-			commandID = `command-${this.idCounter}`;
-		} while (this.isCommandExist(commandID));
+        // form command ID
+        let commandID;
+        do {
+            this.idCounter++;
+            commandID = `command-${this.idCounter}`;
+        } while (this.isCommandExist(commandID));
 
-		return {
-			id: commandID,
-			exec: {
-				label,
-				component,
-				commandLine,
-				workingDir: '${PROJECT_SOURCE}'
-			}
-		};
-	}
+        return {
+            id: commandID,
+            exec: {
+                label,
+                component,
+                commandLine,
+                workingDir: '${PROJECT_SOURCE}'
+            }
+        };
+    }
 
-	/**
-	 * Asks user for the command label
-	 */
-	private async enterLabel(): Promise<string> {
-		return await vscode.window.showInputBox({
-			value: 'Sample Command',
-			title: 'Add Command Label',
+    /**
+     * Asks user for the command label
+     */
+    private async enterLabel(): Promise<string> {
+        return await vscode.window.showInputBox({
+            value: 'Sample Command',
+            title: 'Add Command Label',
 
-			validateInput: (value): string | vscode.InputBoxValidationMessage | undefined | null |
-				Thenable<string | vscode.InputBoxValidationMessage | undefined | null> => {
+            validateInput: (value): string | vscode.InputBoxValidationMessage | undefined | null |
+                Thenable<string | vscode.InputBoxValidationMessage | undefined | null> => {
 
-				if (!value) {
-					return {
-						message: 'Command label cannot be empty',
-						severity: vscode.InputBoxValidationSeverity.Error
-					} as vscode.InputBoxValidationMessage;
-				}
+                if (!value) {
+                    return {
+                        message: 'Command label cannot be empty',
+                        severity: vscode.InputBoxValidationSeverity.Error
+                    } as vscode.InputBoxValidationMessage;
+                }
 
-				const commands = this.service.getDevfile().commands;
-				if (commands) {
-					for (const c of commands) {
-						if (c.exec.label && c.exec.label === value) {
-							return {
-								message: 'A command with this label alredy exists',
-								severity: vscode.InputBoxValidationSeverity.Error
-							} as vscode.InputBoxValidationMessage;
-						}
-					}
+                const commands = this.service.getDevfile().commands;
+                if (commands) {
+                    for (const c of commands) {
+                        if (c.exec.label && c.exec.label === value) {
+                            return {
+                                message: 'A command with this label alredy exists',
+                                severity: vscode.InputBoxValidationSeverity.Error
+                            } as vscode.InputBoxValidationMessage;
+                        }
+                    }
 
-				}
+                }
 
-				return undefined;
-			}
+                return undefined;
+            }
 
-		});
-	}
+        });
+    }
 
-	/**
-	 * Asks user for the component to run
-	 */
-	private async selectComponent(): Promise<string> {
-		const componentNames: string[] = [];
-		for (const c of this.service.getDevfile().components) {
-			// take only container component
-			if (c.container) {
-				componentNames.push(c.name);
-			}
-		}
+    /**
+     * Asks user for the component to run
+     */
+    private async selectComponent(): Promise<string> {
+        const componentNames: string[] = [];
+        for (const c of this.service.getDevfile().components) {
+            // take only container component
+            if (c.container) {
+                componentNames.push(c.name);
+            }
+        }
 
-		return await vscode.window.showQuickPick(componentNames, {
-			title: 'Select a component in which the command will be executed',
-		});
-	}
+        return await vscode.window.showQuickPick(componentNames, {
+            title: 'Select a component in which the command will be executed',
+        });
+    }
 
-	/**
-	 * Asks user to enter command line
-	 */
-	private async enterCommandLine(): Promise<string> {
-		return await vscode.window.showInputBox({
-			value: 'echo "${WELCOME}"',
-			title: 'Enter command line to be executed',
+    /**
+     * Asks user to enter command line
+     */
+    private async enterCommandLine(): Promise<string> {
+        return await vscode.window.showInputBox({
+            value: 'echo "${WELCOME}"',
+            title: 'Enter command line to be executed',
 
-			validateInput: (value): string | vscode.InputBoxValidationMessage | undefined | null |
-				Thenable<string | vscode.InputBoxValidationMessage | undefined | null> => {
+            validateInput: (value): string | vscode.InputBoxValidationMessage | undefined | null |
+                Thenable<string | vscode.InputBoxValidationMessage | undefined | null> => {
 
-				if (!value) {
-					return {
-						message: 'Command line cannot be empty',
-						severity: vscode.InputBoxValidationSeverity.Error
-					} as vscode.InputBoxValidationMessage;
-				}
+                if (!value) {
+                    return {
+                        message: 'Command line cannot be empty',
+                        severity: vscode.InputBoxValidationSeverity.Error
+                    } as vscode.InputBoxValidationMessage;
+                }
 
-				return undefined;
-			}
+                return undefined;
+            }
 
-		});
-	}
+        });
+    }
 
-	private isCommandExist(id: string): boolean {
-		const devfile = this.service.getDevfile();
-		if (!devfile.commands) {
-			return false;
-		}
+    private isCommandExist(id: string): boolean {
+        const devfile = this.service.getDevfile();
+        if (!devfile.commands) {
+            return false;
+        }
 
-		for (const command of devfile.commands) {
-			if (command.id === id) {
-				return true;
-			}
-		}
+        for (const command of devfile.commands) {
+            if (command.id === id) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
 }
