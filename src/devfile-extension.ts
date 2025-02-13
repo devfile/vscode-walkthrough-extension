3c729f31d48542f9a858d7258a3876e7000487a9
Vitaliy Gulyy <vgulyy@redhat.com>
2025-02-13 15:14:28 +0200

    fix: prevent popup disappearing (#38)
    Signed-off-by: vitaliy-guliy <vgulyy@redhat.com>

--- src/devfile-extension.ts
+++ src/devfile-extension.ts
@@ -17,6 +17,28 @@ import { initBindings } from './bindings';
 import { InstallYaml } from './command/install-yaml';
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    // Due to the bug in the upstream https://github.com/microsoft/vscode/issues/214787 it is not possible to show
+    // several sequential popups. To prevent popup disappear it needs to add a small delay between two popups.
+
+    // Keep the original functions
+    const _showQuickPick = vscode.window.showQuickPick;
+    const _showInputBox = vscode.window.showInputBox;
+    
+    // Replace with functions with a small delay 
+    Object.assign(vscode.window, {
+        showQuickPick: async (items, options, token) => {
+            const result = await _showQuickPick(items, options, token);
+            await new Promise(resolve => setTimeout(resolve, 300));
+            return result;
+        },
+
+        showInputBox: async (options, token) => {
+            const result = await _showInputBox(options, token);
+            await new Promise(resolve => setTimeout(resolve, 300));
+            return result;
+        }
+    });
+
     const container = initBindings();
     container.get(DevfileExtensionImpl).start(context);
 }
